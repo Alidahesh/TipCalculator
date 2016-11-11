8624a7268a9c0d01b3bccdae84d53031fa2261a3
Ali <alidahesh@yahoo.com>
2016-11-11 11:17:42 -0800

    Initial Commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Ali Dahesh on 2016-11-11.
+//  Copyright © 2016 Ali Dahesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TipCalculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TipCalculator
+//
+//  Created by Ali Dahesh on 2016-11-11.
+//  Copyright © 2016 Ali Dahesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
